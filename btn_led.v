b770464c175f4272ccfbf0813efa401d6fb0c23b
boli <bipinoli90@gmail.com>
2024-09-19 13:22:01 +0200

    updated verilog code. let's test

--- btn_led.v
+++ btn_led.v
@@ -9,8 +9,20 @@ module btn_led (LED1, LED2, LED3, LED4, BTN1, BTN2, BTN3, BTN4);
   input BTN1, BTN2, BTN3, BTN4;
   output reg LED1, LED2, LED3, LED4;
 
-  always @(posedge BTN1)
-    LED1 =~ LED1;
+  reg btn1_state;  // Register to store the previous button state
+
+  // Initialize LED and button state
+  initial begin
+    LED1 = 0;         // Start with LED off
+    btn1_state = 0;   // Initial button state
+  end
+
+  always @(posedge BTN1) begin
+    if (BTN1 != btn1_state) begin
+        LED1 = ~LED1;  // Toggle LED1 when button is pressed
+        btn1_state = BTN1;  // Update button state
+    end
+  end
 
 endmodule
 
