2b634e69b9144974b11b7b461a382159da793575
boli <bipinoli90@gmail.com>
2024-09-19 13:16:16 +0200

    updated verilog and pcf file

--- btn_led.v
+++ btn_led.v
@@ -1,15 +1,16 @@
 //------------------------------------------------------------------
-//-- Hello world example
-//-- Control leds by pushing the buttons
+//-- Pressing a button toggle the state of the led on the side to it 
 //-- This example has been tested on the following boards:
-//--   * iCE40-HX1K-EVB Olimex
+//     * iCE40HX1K-BLINK-EVN
+//-- By: Bipin Oli (https://github.com/Bipinoli)
 //------------------------------------------------------------------
 
-module btn_led(output wire  [1:0] led,
-			input wire [1:0] but
-	);
+module btn_led (LED1, LED2, LED3, LED4, BTN1, BTN2, BTN3, BTN4);
+  input BTN1, BTN2, BTN3, BTN4;
+  output reg LED1, LED2, LED3, LED4;
 
-assign led[0]=but[0];
-assign led[1]=but[1];
+  always @(posedge BTN1)
+    LED1 =~ LED1;
 
 endmodule
+
